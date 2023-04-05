b46fe9e3fceb9092b8b91ecd3ffeb8b26780646f
bekzat <imashevbekzat77@gmail.com>
2023-04-05 15:28:13 +0300

    added layout and added nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-
+import { Route, Routes } from "react-router";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello world</h1>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<h1>Home</h1>} />
+            <Route path="/about" element={<h1>About</h1>} />
+            <Route path="/contacts" element={<h1>Contacts</h1>} />
+            <Route path="/delivery" element={<h1>Delivery</h1>} />
+
+          </Routes>
+        </Layout>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { BrowserRouter } from 'browser-router';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+   <BrowserRouter>
        <App />
-    </BrowserRouter>
+   </BrowserRouter>
+      
+
+
+  
   </React.StrictMode>
 );
 
