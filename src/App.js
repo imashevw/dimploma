73ab30873d65e1c8d147c7d97d7dfa45d4c31511
bekzat <imashevbekzat77@gmail.com>
2023-04-05 15:46:05 +0300

    added About.js, Contact.js and Home.js for pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router";
 import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
     <div className="App">
         <Layout>
           <Routes>
-            <Route path="/" element={<h1>Home</h1>} />
-            <Route path="/about" element={<h1>About</h1>} />
+            <Route path="/" element={<Home/>} />
+            <Route path="/about" element={<About/>} />
             <Route path="/contacts" element={<h1>Contacts</h1>} />
             <Route path="/delivery" element={<h1>Delivery</h1>} />
 
