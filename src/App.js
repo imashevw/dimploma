60aa335a57b04c8d59a00f33177ebd1f95422bb8
bekzat <imashevbekzat77@gmail.com>
2023-04-07 10:48:03 +0300

    added display grid on layout, and added delivery js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router";
 import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Contacts from "./pages/Contacts";
+import Delivery from "./pages/Delivery";
 
 function App() {
   return (
@@ -10,8 +12,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/about" element={<About/>} />
-            <Route path="/contacts" element={<h1>Contacts</h1>} />
-            <Route path="/delivery" element={<h1>Delivery</h1>} />
+            <Route path="/contacts" element={<Contacts/>} />
+            <Route path="/delivery" element={<Delivery/>} />
 
           </Routes>
         </Layout>
