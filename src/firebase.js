8b660951e6a6dce02bb47960963686f55489e06a
bekzat <imashevbekzat77@gmail.com>
2023-04-14 22:11:09 +0300

    import firestore and export categoryCollection, productsCollection in firebase js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection } from 'firebase/firestore/lite';
+import { getFirestore, collection } from "firebase/firestore/lite";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -10,10 +10,11 @@ const firebaseConfig = {
     storageBucket: "dimplom-fc21c.appspot.com",
     messagingSenderId: "1071946100983",
     appId: "1:1071946100983:web:f25c0b6b89d1d659b163f2"
-  };
+};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+//
 const db = getFirestore(app);
 
-export const categories = collection(db, "categories");
-
+export const categoryCollection = collection(db, "categories");
+export const productsCollection = collection(db, "products");
