2ad6d8686983124808724079dbb157f2c8e0ed1e
bekzat <imashevbekzat77@gmail.com>
2023-04-15 20:19:31 +0300

    download some picture for carousel

---
+++ src/components/Carousel/CarouselBox.js
@@ -0,0 +1,43 @@
+// import { Component } from "react";
+// import Carousel from 'react-bootstrap/Carousel'
+// import windbreakerImg from "../assets/windbreaker.jpg"
+// import suitClassicImg from "../assets/suit-classic.jpg"
+// import thousesImg from "../assets/thouses.jpg"
+// export default class CarouselBox extends Component {
+//     render() {
+//         return (
+//             <Carousel>
+//                 <Carousel.Item>
+//                     <img className="windbreaker"
+//                     src={windbreakerImg}
+//                     alt="clothes"
+//                     />
+//                     <Carousel.Caption>
+//                         <h3>скидки 50% в честь твоего дня рождении</h3>
+//                         <p>dlankfsjd as djkfadj akj k sakdfa</p>
+//                     </Carousel.Caption>
+//                 </Carousel.Item>
+//                 <Carousel.Item>
+//                     <img className="suitClassic"
+//                     src={suitClassicImg}
+//                     alt="clothes"
+//                     />
+//                     <Carousel.Caption>
+//                         <h3>скидки 50% в честь твоего дня рождении</h3>
+//                         <p>dlankfsjd as djkfadj akj k sakdfa</p>
+//                     </Carousel.Caption>
+//                 </Carousel.Item>
+//                 <Carousel.Item>
+//                     <img className="thouses"
+//                     src={thousesImg }
+//                     alt="clothes"
+//                     />
+//                     <Carousel.Caption>
+//                         <h3>скидки 50% в честь твоего дня рождении</h3>
+//                         <p>dlankfsjd as djkfadj akj k sakdfa</p>
+//                     </Carousel.Caption>
+//                 </Carousel.Item>
+//             </Carousel>
+//         )
+//     }
+// }
