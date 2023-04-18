c4a07e0dd9cc07eed439fee4c62af8ef8dad3ada
bekzat <imashevbekzat77@gmail.com>
2023-04-18 20:41:26 +0300

    created Cart.js and connected CartList.js in Cart.js

---
+++ src/pages/Cart.js
@@ -0,0 +1,10 @@
+import CartList from "../components/CartList/CartList";
+
+export default function Cart() {
+    return (
+        <div className="Cart">
+            <h1>CART</h1>
+            <CartList/>
+        </div>
+    );
+}
