9f1c2fe0dc2e3dc33b39063efe823d80fb77c003
bekzat <imashevbekzat77@gmail.com>
2023-04-18 19:35:39 +0300

    created CartList.js to show a cart with a picture of a product and a price

---
+++ src/components/CartList/CartList.js
@@ -0,0 +1,41 @@
+import { useContext } from "react";
+import "./CartList.css";
+import { AppContext } from "../../App";
+import { Link } from "react-router-dom";
+
+export default function() {
+    const {products, cart, setCart} = useContext(AppContext);
+
+    function onQuantityChange(product, qty) {
+        setCart({
+            ...cart,
+            [product.id]: qty,
+        });
+    }
+
+    function onItemRemove(product) {
+        const newCart = {...cart};
+        delete newCart[product.id];
+        setCart(newCart);
+    }
+
+
+    const productIds = Object.keys(cart);
+
+    const output = products
+    .filter((product) => productIds.includes(product.id))
+    .map((product) => (
+        <div className="CartItem" key={product.id}>
+            <img src={product.picture} alt={product.name} />
+            <Link to={"/product/" + product.slug}>{product.name}</Link>
+            {cart[product.id]}
+            <input type="number" value={cart[product.id]} min={1} 
+                onChange={(event) => onQuantityChange(product, +event.target.value)}/>
+            <span>{cart[product.id] * product.price} som</span>
+            <button onClick={() => onItemRemove(product)}>Remove</button>
+        </div>
+    ));
+    return (
+        <div className="CartList">{output}</div>
+    )
+}
