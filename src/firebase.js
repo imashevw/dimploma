54f83ace72af5a4fae834ff974e5c2900ea7c40a
bekzat <imashevbekzat77@gmail.com>
2023-05-12 13:59:59 +0300

    added functionality to firebase so that it would download photos and save them to firebase

--- src/firebase.js
+++ src/firebase.js
@@ -8,6 +8,7 @@ import {
   signInWithPopup,
   signOut,
 } from "firebase/auth";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -24,6 +25,10 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
 const auth = getAuth(app);
+
+export const storage = getStorage(app);
+
+
 
 export const categoryCollection = collection(db, "categories");
 export const productsCollection = collection(db, "products");
@@ -43,6 +48,7 @@ export const onCategoriesLoad = (callback) =>
       }))
     )
   );
+
 export const onProductsLoad = (callback) =>
   onSnapshot(productsCollection, (snapshot) =>
     callback(
@@ -52,6 +58,7 @@ export const onProductsLoad = (callback) =>
       }))
     )
   );
+
 export const onOrdersLoad = (callback) =>
   onSnapshot(ordersCollection, (snapshot) =>
     callback(
@@ -61,3 +68,11 @@ export const onOrdersLoad = (callback) =>
       }))
     )
   );
+
+  export const uploadProductPhoto = async (file) => {
+    const storageRef = ref(storage, `products/${file.name}`);
+    await uploadBytes(storageRef, file);
+  
+    const url = await getDownloadURL(storageRef);
+    return url;
+  };
