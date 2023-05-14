a91455f53f5bab180e515ec3d342d94ae7792fae
bekzat <imashevbekzat77@gmail.com>
2023-05-14 15:02:05 +0300

    added a background image on the main page of my site that would be beautiful and wanted to use minimalism

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,16 @@
+import backgroundImage from './.././assets/home.jpg';
+
 export default function Home() {
-    return (
-        <div className="Home">
-            
-        </div>
-    )
+  const styles = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '100vh'
+  };
+
+  return (
+    <div className="Home" style={styles}>
+      {/* Content */}
+    </div>
+  );
 }
