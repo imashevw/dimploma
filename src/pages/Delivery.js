7e023e507e28954a1e94118e891cc44065939804
bekzat <imashevbekzat77@gmail.com>
2023-05-17 14:33:26 +0300

    connected the footer inside the footer

--- src/pages/Delivery.js
+++ src/pages/Delivery.js
@@ -1,8 +1,9 @@
+import DeliveryInfo from "../components/DeliveryInfo/DeliveryInfo";
+
 export default function Delivery() {
-    return (
-        <div className="Delivery">
-        <h1>Delivery</h1>
-        <p>This is the delivery page</p>
-        </div>
-    )
+  return (
+    <div className="Delivery">
+      <DeliveryInfo />
+    </div>
+  );
 }
