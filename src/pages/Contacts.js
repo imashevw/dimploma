d2a546dc0fe315f2fa5d1c12fff14e1f025ce467
bekzat <imashevbekzat77@gmail.com>
2023-05-18 15:00:23 +0300

    change

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,8 +1,7 @@
 export default function Contacts() {
     return (
         <div className="Contacts">
-         <h1>Contacts</h1>
-         <p>This is the contacts page</p>
+            
         </div>
     )
 }
