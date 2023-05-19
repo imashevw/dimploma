6efe9adbfce473bf8f5fc6e6e9150c1ebdf3c205
bekzat <imashevbekzat77@gmail.com>
2023-05-19 14:55:17 +0300

    created the About component and added information about our site inside and set the style

---
+++ src/components/AboutInfo/AboutInfo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './AboutInfo.css';
+
+export default function AboutInfo() {
+  return (
+    <div className="AboutInfo">
+      <h2>About Our Online Store</h2>
+      <p>Welcome to our online clothing store! We offer a wide range of trendy and fashionable clothing for men, women, and children.</p>
+      <p>At our store, we believe that fashion should be accessible to everyone. That's why we provide high-quality clothing at affordable prices.</p>
+      <p>Whether you're looking for casual wear, formal attire, or stylish accessories, we have you covered. Our collection includes a variety of styles, sizes, and designs to suit your preferences.</p>
+      <p>Shop with confidence knowing that we prioritize customer satisfaction. We strive to provide excellent customer service and ensure a seamless shopping experience.</p>
+      <p>Stay updated with the latest fashion trends by subscribing to our newsletter. Don't miss out on exclusive deals, promotions, and new arrivals!</p>
+      <p>Thank you for choosing our online store. Happy shopping!</p>
+    </div>
+  );
+}
