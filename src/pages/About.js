5df179b5898da97764d78c518995225e2e9ae682
bekzat <imashevbekzat77@gmail.com>
2023-05-19 15:01:25 +0300

    added the ContactsInfo component set the style

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,5 @@
+import AboutInfo from "../components/AboutInfo/AboutInfo";
+
 export default function About() {
-  return (
-    <div className="About">
-      <h1>About</h1>
-      <p>This is the about page</p>
-    </div>
-  );
+  return <AboutInfo />;
 }
